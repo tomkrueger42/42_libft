44fed78342f714fa0d904da4ca6de812e4142dce
Tom Krüger <tom@joloto.de>
2021-12-16 15:25:50 +0100

    changed return value for ft_putstr_fd()

--- src/put/ft_putstr_fd.c
+++ src/put/ft_putstr_fd.c
@@ -6,20 +6,20 @@
 /*   By: tomkrueger <tomkrueger@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/05 18:56:46 by tomkrueger        #+#    #+#             */
-/*   Updated: 2021/12/16 15:06:51 by tomkrueger       ###   ########.fr       */
+/*   Updated: 2021/12/16 15:24:19 by tomkrueger       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/libft.h"
 
 /* This function writes a string to filedescriptor fd */
-void	ft_putstr_fd(char *s, int fd)
+int	ft_putstr_fd(char *s, int fd)
 {
 	int	i;
 
 	i = 0;
 	if (!s)
-		return ;
+		return (0);
 	while (*s)
 		i += write(fd, s++, 1);
 	return (i);
