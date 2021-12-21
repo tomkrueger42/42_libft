a432fa74cff6ce9c79ba3c707032af7a29d70173
Tom Kruger <tkruger@1-A-3.42heilbronn.de>
2021-12-21 23:11:39 +0100

    fixed ft_free_all() and ft_free()

--- src/mem/ft_free.c
+++ src/mem/ft_free.c
@@ -6,7 +6,7 @@
 /*   By: tkruger <tkruger@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/21 22:54:12 by tkruger           #+#    #+#             */
-/*   Updated: 2021/12/21 23:08:26 by tkruger          ###   ########.fr       */
+/*   Updated: 2021/12/21 23:10:28 by tkruger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,12 @@
 /* This f() frees a va_list of pointers */
 void	ft_free(int count, va_list pointers)
 {
-	void	*ptr;
+	unsigned long	ptr;
 
 	while (count-- > 0)
 	{
 		ptr = va_arg(pointers, unsigned long);
 		if (ptr != NULL)
-			free(ptr);
+			free((uintptr_t)ptr);
 	}
 }
--- src/mem/ft_free_all.c
+++ src/mem/ft_free_all.c
@@ -6,7 +6,7 @@
 /*   By: tkruger <tkruger@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/21 22:31:36 by tkruger           #+#    #+#             */
-/*   Updated: 2021/12/21 23:08:18 by tkruger          ###   ########.fr       */
+/*   Updated: 2021/12/21 23:10:55 by tkruger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,15 @@
 /* This f() frees all pointers passed to it as arguments */
 void ft_free_all(int count, ...)
 {
-	va_list	pointers;
-	void	*ptr;
+	va_list			pointers;
+	unsigned long	*ptr;
 
 	va_start(pointers, count);
 	while (count-- > 0)
 	{
 		ptr = va_arg(pointers, unsigned long);
 		if (ptr != NULL)
-			free(ptr);
+			free((uintptr_t)ptr);
 	}
 	va_end(pointers);
 }
